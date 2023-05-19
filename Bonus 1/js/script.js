cb7d2027128596061b1986950182e286fae27408
CraciDenisStefan <crdenis13@gmail.com>
2023-05-19 19:39:14 +0200

    [FIX]Bonus 1 completo , fix con append .

--- Bonus 1/js/script.js
+++ Bonus 1/js/script.js
@@ -5,24 +5,27 @@
     if( (i % 3 === 0) && (i % 5 === 0) ){
         let i = 'FizzBuzz'
         console.log(i);
-        let lista = document.getElementById(`list`)
-        lista.innerHTML += `<li>${i}</li>`
+        let lista = document.createElement("li");
+        lista.innerHTML = (i);
+        document.querySelector(".container").append(lista);  
         }else if ( i % 3 === 0){
             let i = 'Fizz';
             console.log(i);
-            let lista = document.getElementById(`list`)
-            lista.innerHTML += `<li>${i}</li>`
+            let lista = document.createElement("li");
+            lista.innerHTML = (i);
+            document.querySelector(".container").append(lista);  
             }else if ( i % 5 === 0 ){
                 let i = 'Buzz';
                 console.log(i);
-                let lista = document.getElementById(`list`)
-                lista.innerHTML += `<li>${i}</li>`
+                let lista = document.createElement("li");
+                lista.innerHTML = (i);
+                document.querySelector(".container").append(lista);  
                 }else{
                     console.log(i)
-                    let lista = document.getElementById(`list`)
-                    lista.innerHTML += `<li>${i}</li>`
+                    let lista = document.createElement("li");
+                    lista.innerHTML = (i);
+                    document.querySelector(".container").append(lista);  
                 }
-            
-} 
 
-
+         
+}
