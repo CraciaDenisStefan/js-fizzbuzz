647f43bb9abc02cc94e4be65dfd487d2a80067d5
CraciDenisStefan <crdenis13@gmail.com>
2023-05-19 17:48:34 +0200

    Esercizio completato

---
+++ js/script.js
@@ -0,0 +1,19 @@
+"use strict";
+ 
+ for (let i = 1; i <= 100; i++){
+
+    if( (i % 3 === 0) && (i % 5 === 0) ){
+        let i = 'FizzBuzz'
+        console.log(i);
+        }else if ( i % 3 === 0){
+            let i = 'Fizz';
+            console.log(i);
+            }else if ( i % 5 === 0 ){
+                let i = 'Buzz';
+                console.log(i);
+                }else{
+                    console.log(i)
+                }
+} 
+
+
