813291304d060b3b0999a7d2c4a8ebb84423dc06
CraciDenisStefan <crdenis13@gmail.com>
2023-05-21 17:27:51 +0200

    Bonus 2

---
+++ Bonus 2/js/script.js
@@ -0,0 +1,29 @@
+"use strict";
+ 
+ for (let i = 1; i <= 100; i++){
+    let output = ``;
+   
+    if (i % 3 === 0){
+        output = `Fizz`
+        let colonna = document.querySelector(".row");
+        colonna.innerHTML += ` <div class="col-1 m-3 fizz p-3 ">${output}</div>`;
+    }
+    if (i % 5 === 0){
+        output = `Buzz`
+        let colonna = document.querySelector(".row");
+        colonna.innerHTML += ` <div class="col-1 m-3 buzz p-3 ">${output}</div>`;
+    }
+    if( (i % 3 === 0) && (i % 5 === 0) ){
+        output = `FizzBuzz`
+        let colonna = document.querySelector(".row");
+        colonna.innerHTML += ` <div class="col-1 m-3 fizzbuzz p-3  ">${output}</div>`;
+    }
+    if(output === ``){
+        let colonna = document.querySelector(".row");
+        colonna.innerHTML += ` <div class="col-1 m-3 p-3 ">${i}</div>`;
+        output = i
+    }
+        console.log(output) 
+     
+       
+}
